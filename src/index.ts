4ff9e6e1513be40beec6273bb2f1e9ba59228cf0
Sammwy <sammwy.dev@gmail.com>
2021-09-23 14:28:50 -0300

    Created http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello World')
+import dotenv from 'dotenv'
+import express from 'express'
+import mongoose from 'mongoose'
+
+// Setup .env file
+dotenv.config()
+
+// Connect to database
+mongoose.connect(process.env['MONGODB_URI']).then(() => {
+  console.log('MongoDB database connected.')
+})
+
+// Initialize http server
+const app = express()
+
+app.listen(process.env['PORT'], () => {
+  console.log('Application listening at port ' + process.env['PORT'])
+})
